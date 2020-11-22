20c3d21b4807409bde03e74a7f79d765830f7846
Kristina Stojic <kristinastojic99@uns.ac.rs>
2020-11-22 13:09:05 +0100

    #glavni prozor
    Napravljen glavni prozor sa potrebnim dimenzijama i u njemu pozvan Menu bar.

--- src/izgledAplikacije/Glavni_prozor.java
+++ src/izgledAplikacije/Glavni_prozor.java
@@ -1,5 +1,35 @@
 package izgledAplikacije;
 
-public class Glavni_prozor {
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
+
+
+
+public class Glavni_prozor extends JFrame {
+
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public Glavni_prozor() {
+    	Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+    	int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        
+        setSize(screenWidth / 4*3  , screenHeight / 4*3);
+        setTitle("Studentska služba");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        MenuBar menu = new MenuBar();
+		this.setJMenuBar(menu);
+    	
+    }
+    
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import izgledAplikacije.Glavni_prozor;
+
 public class Main {
 	public static void main(String args[]) {
+		
+		Glavni_prozor gp = new Glavni_prozor();
+		gp.setVisible(true);
 		
 	}
 }
