2b524ff1e928d1802dda69cc5de71343e8be3719
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-04 21:57:12 +0100

    #help

--- src/izgledAplikacije/Help.java
+++ src/izgledAplikacije/Help.java
@@ -18,7 +18,7 @@ public class Help extends JDialog{
 		String tekst = "U glavni prozor aplikacije dodati su menu bar, status bar i toolbar.\n"
 					   + "Meni bar sadrzi:\n"
 					   + "  -Podmeni File(akcelerator: alt+f). U kome se nalaze stavke: \n"
-					   + "    New(mnemonik: ctrl+new): sluzi za dodavanje novog entiteta u sistem. \n"
+					   + "    New(mnemonik: ctrl+n): sluzi za dodavanje novog entiteta u sistem. \n"
 					   + "    Close(mnemonik: ctrl+c): sluzi za zatvaranje aplikacije.\n"
 					   + "  -Podmeni Edit(akcelerator: alt+e). U kome se nalaze stavke: \n"
 					   + "    Edit(mnemonik: ctrl+e): sluzi za izmjenu postojeceg entiteta\n"
@@ -28,7 +28,7 @@ public class Help extends JDialog{
 					   + "    About(mnemonik: ctrl+a): sluzi za prikaz verzije aplikacije. I sadrzi sazetu biografiju autora.\n"
 					   + "Toolbar sadrzi 4 dugmeta i jedno polje za unos teksta:\n"
 					   + "  1 dugme(akcelerator alt+n): sluzi za otvaranje dijaloga i kreiranje entiteta informacionog sistema\n"
-					   + "  2 dugme(akcelerator alt+i): sluzi za otvaranje dijaloga i izmjenu oznacenog entiteta\n"
+					   + "  2 dugme(akcelerator alt+e): sluzi za otvaranje dijaloga i izmjenu oznacenog entiteta\n"
 					   + "  3 dugme(akcelerator alt+d): sluzi da obrise oznaceni entitet iz sistema\n"
 					   + "  U tekstualno polje za pretragu korisnik moze unijeti kriterijum pretrage i pritiskom na dugme za pretragu pretraziti tabelu aktivnog taba\n"
 					   + "Status bar aplikacije sadrzi: naziv aplikacije, trenutno vrijeme i trenutni datum.";
