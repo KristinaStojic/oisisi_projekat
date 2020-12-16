88347f208c14bb78b99b5b445b73362b5bfb34bf
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-16 22:04:13 +0100

    #prikaz_studenata
    Napravljene klase JTables i MyTabbedPane, u njih dodate osnovne stvari za prikaz studenata

--- src/izgledAplikacije/MyTabbedPane.java
+++ src/izgledAplikacije/MyTabbedPane.java
@@ -1,5 +1,9 @@
 package izgledAplikacije;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
 public class MyTabbedPane extends JTabbedPane{
@@ -9,10 +13,26 @@ public class MyTabbedPane extends JTabbedPane{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	enum Tab {Studenti, Profesori, Predmeti};
+	
+	JPanel studentiPanel = new JPanel();
+	JPanel profesoriPanel = new JPanel();
+	JPanel predmetiPanel = new JPanel();
+	
+	public static Tab tab = Tab.Studenti;
+	
+	static JTables studentiTabela;
+	static JTables profesoriTabela;
+	static JTables predmetiTabela;
+	
 	public MyTabbedPane() {
-		super.addTab("Student", null);
-		super.addTab("Profesor", null);
-		super.addTab("Predmet", null);
+		studentiPanel.setLayout(new BorderLayout());
+		studentiTabela = new JTables(Tab.Studenti);
+		JScrollPane pane = new JScrollPane(studentiTabela);
+		studentiPanel.add(pane, BorderLayout.CENTER);
+		add(Tab.Studenti.toString(), studentiPanel);
+		
+		
 	}
 	
 	
