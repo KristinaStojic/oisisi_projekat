129dbdb0616326f0d9080fa3ee90c302830e6e9b
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-16 23:28:21 +0100

    #about

---
+++ src/dialozi/About.java
@@ -0,0 +1,35 @@
+package dialozi;
+
+import java.awt.Dimension;
+
+import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+
+import izgledAplikacije.Glavni_prozor;
+
+public class About extends JDialog{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public About(Glavni_prozor gp) {
+		super(gp,"About",true);
+		JTextPane txt = new JTextPane();
+		String tekst = "Verzija: 2020/2021\n"
+						+ "Aplikacija studentska sluzba\n"
+						+ "Student 1:\n"
+						+ "Katarina Žerajić\n"
+						+ "Rođena 20.10.1999 u Nevesinju. Student treće godine racunarstva i automatike.\n"
+						+ "Student 2:\n"
+						+ "Kristina Stojić\n"
+						+ "Rođena 5.03.1999 u Zvorniku. Student treće godine racunarstva i automatike.";
+		txt.setText(tekst);
+		txt.setEditable(false);
+		add(txt);
+		add(new JScrollPane(txt));
+		setSize(new Dimension(500,400));
+		setLocationRelativeTo(null);		
+	}
+}
