88272b855c2902cbe8a7b4197f77fa175f19cbf6
Kristina Stojic <kristinastojic99@uns.ac.rs>
2020-12-16 22:02:44 +0100

    #prikaz_predmeta
    Implementirana klasa AbstractTableModelPredmeti i dodate metode u klasi BazaPredmeta.

--- src/izgledAplikacije/AbstractTableModelPredmeti.java
+++ src/izgledAplikacije/AbstractTableModelPredmeti.java
@@ -1,6 +1,30 @@
 package izgledAplikacije;
 
-public class AbstractTableModelPredmeti {
+import javax.swing.table.AbstractTableModel;
+
+import model.BazaPredmeta;
+
+public class AbstractTableModelPredmeti extends AbstractTableModel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public int getRowCount() {
+		return BazaPredmeta.getInstance().getPredmeti().size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return BazaPredmeta.getInstance().getColumnCount();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return BazaPredmeta.getInstance().getValueAt(rowIndex, columnIndex);
+	}
 
 	
 	
