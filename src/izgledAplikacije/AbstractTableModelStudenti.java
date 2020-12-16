01b624a7f4e6c70f9233881fba4c8f383f33a395
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-16 21:23:50 +0100

    #dodavanje_studenta
    Napravljena klasa abstractTableModelStudenti

---
+++ src/izgledAplikacije/AbstractTableModelStudenti.java
@@ -0,0 +1,36 @@
+package izgledAplikacije;
+
+import javax.swing.table.AbstractTableModel;
+
+import model.BazaStudenata;
+
+public class AbstractTableModelStudenti extends AbstractTableModel{
+
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+		public AbstractTableModelStudenti() {}
+		
+		@Override
+		public int getRowCount() {
+			return BazaStudenata.getInstance().getStudenti().size();
+		}
+
+		@Override
+		public int getColumnCount() {
+			return BazaStudenata.getInstance().getColumnCount();
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			return BazaStudenata.getInstance().getColumnName(column);
+		}
+		
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			return BazaStudenata.getInstance().getValueAt(rowIndex, columnIndex);
+		}
+		
+}
