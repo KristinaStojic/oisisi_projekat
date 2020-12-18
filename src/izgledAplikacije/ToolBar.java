11f9152b688017748567dfead704a03b61712ac3
Kristina Stojic <kristinastojic99@uns.ac.rs>
2020-12-18 09:47:00 +0100

    #toolbar
    Promenjen naziv fajla.

--- src/izgledAplikacije/ToolBar.java
+++ src/izgledAplikacije/ToolBar.java
@@ -14,7 +14,7 @@ import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 
-public class Tool_bar extends JToolBar{
+public class ToolBar extends JToolBar{
 	
 	/**
 	 * 
