6e080f14f211592de1d7f4d94ecfd65dc37655d4
Kristina Stojic <kristinastojic99@uns.ac.rs>
2020-12-23 19:25:47 +0100

    #dodavanje_predmeta
    Realizovan prozor dodavanje predmeta, i povezani menu i toolbar.

--- src/izgledAplikacije/AddAction.java
+++ src/izgledAplikacije/AddAction.java
@@ -37,6 +37,11 @@ public class AddAction extends AbstractAction{
 					addProfessor.setVisible(true);
 					
 				}
+				else if(GlavniProzor.getInstance().tabbedPane.getSelectedIndex() == 2) {
+					AddPredmet addPredmet = new AddPredmet();
+					addPredmet.setVisible(true);
+					
+				}
 
 	}
 	
