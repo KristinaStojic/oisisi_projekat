ffd546f12245909f47055ed49080ead8ea06dcd1
Kristina Stojic <kristinastojic99@uns.ac.rs>
2020-12-24 14:55:28 +0100

    #prikaz_predmeta
    Omogucen prikaz predmeta

--- src/izgledAplikacije/JTables.java
+++ src/izgledAplikacije/JTables.java
@@ -25,6 +25,12 @@ public class JTables extends JTable{
 				AbstractTableModelProfesori modelProfesori = new AbstractTableModelProfesori();
 				this.setModel(modelProfesori);
 				break;
+				
+		case Predmeti:
+			AbstractTableModelPredmeti modelPredmeti= new AbstractTableModelPredmeti();
+			this.setModel(modelPredmeti	);
+			break;
+		
 		}
 	}
 	
