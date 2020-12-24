6875da788d9dc39bcd6db9ed6417924fcce6eb06
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-24 12:14:33 +0100

    Revert "#prikaz_predmeta"
    This reverts commit 4f4cfcf7c37bf60e2b6b56bd6573cab6f7e9c09f.

--- src/izgledAplikacije/JTables.java
+++ src/izgledAplikacije/JTables.java
@@ -25,10 +25,6 @@ public class JTables extends JTable{
 				AbstractTableModelProfesori modelProfesori = new AbstractTableModelProfesori();
 				this.setModel(modelProfesori);
 				break;
-		case Predmeti:
-			AbstractTableModelPredmeti modelPredmeti= new AbstractTableModelPredmeti();
-			this.setModel(modelPredmeti	);
-			break;
 		}
 	}
 	
