2b389118deb9f69a47ff8fdd483672b93ef9dde6
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-24 11:35:53 +0100

    #dodavanje_studenta
    Realizovane provjere za unos indeksa i godine upisa.

--- src/listeneri/MyFocusListener2.java
+++ src/listeneri/MyFocusListener2.java
@@ -18,11 +18,12 @@ public class MyFocusListener2 implements FocusListener{
 	@Override
 	public void focusLost(FocusEvent e) {
 		JTextField txt = (JTextField) e.getComponent();
-		Pattern pattern = Pattern.compile("[0-9]{5}[/][0-9]{2}[-][0-9]{3}[-][0-9]{2}[-][0-9]{2}");
+		Pattern pattern = Pattern.compile("[0-9]{9,10}");
 		
 		if(!pattern.matcher(txt.getText()).matches()) {
-			JOptionPane.showMessageDialog(null, "Molimo unesite ispravan broj telefona u formatu '00381/62-111-11-11'!");
+			JOptionPane.showMessageDialog(null, "Molimo unesite ispravan broj telefona u formatu '062111111'!");
 			txt.setText("");
+			txt.requestFocus();
 		}
 	}
 
--- src/listeneri/MyFocusListener4.java
+++ src/listeneri/MyFocusListener4.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class MyFocusListener1 implements FocusListener {
+public class MyFocusListener4 implements FocusListener{
 
 	@Override
 	public void focusGained(FocusEvent e) {
@@ -18,13 +18,13 @@ public class MyFocusListener1 implements FocusListener {
 	@Override
 	public void focusLost(FocusEvent e) {
 		JTextField txt = (JTextField) e.getComponent();
-		Pattern pattern = Pattern.compile("[0-3][0-9][.](0[1-9]|1[012])[.][0-2][0-9][0-9][0-9]");
+		Pattern pattern = Pattern.compile("[0-9]{4}");
 		
 		if(!pattern.matcher(txt.getText()).matches()) {
-			JOptionPane.showMessageDialog(null, "Molimo unesite ispravan datum u formatu 'dd-mm-yyyy'!");
+			JOptionPane.showMessageDialog(null, "Molimo unesite ispravnu godinu!");
 			txt.setText("");
 			txt.requestFocus();
 		}
 	}
-
+	
 }
