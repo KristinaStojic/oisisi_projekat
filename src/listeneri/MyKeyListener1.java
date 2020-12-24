806644119927ba0c9cb6e795e00413ad4281ad18
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-24 10:36:57 +0100

    #dodavanje_studenta
    Realizovana provjera za unos imena i prezimena.

--- src/listeneri/MyKeyListener1.java
+++ src/listeneri/MyKeyListener1.java
@@ -2,6 +2,7 @@ package listeneri;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -11,20 +12,7 @@ public class MyKeyListener1 implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		
-				
-		if (e.isActionKey() || e.getKeyCode() == KeyEvent.VK_ENTER
-				|| e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-			return;
-		}
 		
-		char c = e.getKeyChar();
-		
-		if (!Character.isLetter(c) && !(c ==KeyEvent.VK_BACK_SPACE) && !(c == KeyEvent.VK_SPACE) && !(c == KeyEvent.VK_CAPS_LOCK)) {
-			JOptionPane.showMessageDialog(null, "Dozvoljen je unos samo slova!");
-			//JTextField txt = (JTextField) e.getComponent();
-			//txt.setText(txt.getText().substring(0, txt.getText().length()));
-		
-		}
 		
 	}
 
@@ -44,7 +32,19 @@ public class MyKeyListener1 implements KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-
+		
+		if (e.isActionKey() || e.getKeyCode() == KeyEvent.VK_ENTER
+				|| e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			return;
+		}
+		
+		JTextField txt = (JTextField) e.getComponent();
+		Pattern pattern = Pattern.compile("[A-Z|a-z]*");
+				
+		if(!pattern.matcher(txt.getText()).matches()) {
+			JOptionPane.showMessageDialog(null, "Dozvoljen je samo unos slova!");
+			txt.setText(txt.getText().substring(0, txt.getText().length() - 1));
+		}
 	}
 
 }
