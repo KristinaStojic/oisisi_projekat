149076866352a3bdd28779d517fd59ea33a15e8e
Kina999 <73175451+Kina999@users.noreply.github.com>
2020-12-24 11:11:28 +0100

    #dodavanje_studenta
    Realizovana provjera za datum rodjenja i broj telefona preko focus listenera.

---
+++ src/listeneri/MyFocusListener1.java
@@ -0,0 +1,29 @@
+package listeneri;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.regex.Pattern;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class MyFocusListener1 implements FocusListener {
+
+	@Override
+	public void focusGained(FocusEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		JTextField txt = (JTextField) e.getComponent();
+		Pattern pattern = Pattern.compile("[0-3][0-9][-](0[1-9]|1[012])[-][0-2][0-9][0-9][0-9]");
+		
+		if(!pattern.matcher(txt.getText()).matches()) {
+			JOptionPane.showMessageDialog(null, "Molimo unesite ispravan datum u formatu 'dd-mm-yyyy'!");
+			txt.setText("");
+		}
+	}
+
+}
