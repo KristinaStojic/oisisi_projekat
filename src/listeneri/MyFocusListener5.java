c8cd0a08bfcab809eecfd2355f92d8e5aeb75a1c
Kristina Stojic <kristinastojic99@uns.ac.rs>
2020-12-27 21:26:11 +0100

    #dodavanje_profesora

--- src/listeneri/MyFocusListener5.java
+++ src/listeneri/MyFocusListener5.java
@@ -19,7 +19,7 @@ public class MyFocusListener5 implements FocusListener {
 	public void focusLost(FocusEvent e) {
 		
 		JTextField txt = (JTextField) e.getComponent();
-		Pattern pattern = Pattern.compile("[A-Z|a-z|ž|Ž|Đ|đ|Š|š|ć|Ć|č|Č_ ]*[0-9]*[,_ ][A-Z|a-z|ž|Ž|Đ|đ|Š|š|ć|Ć|č|Č_ ]*");
+		Pattern pattern = Pattern.compile("[A-Z|a-z|ž|Ž|Đ|đ|Š|š|ć|Ć|č|Č_ ]+[0-9]{0,4}[,_ ][A-Z|a-z|ž|Ž|Đ|đ|Š|š|ć|Ć|č|Č_ ]+");
 
 		if(!pattern.matcher(txt.getText()).matches() && !txt.getText().trim().isEmpty()) {
 			JOptionPane.showMessageDialog(null, "Molimo unesite ispravnu adresu u formatu NAZIV_ULICE BROJ, NAZIV_GRADA");
