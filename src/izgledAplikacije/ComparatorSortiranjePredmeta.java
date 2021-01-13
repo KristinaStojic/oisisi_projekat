a3c0b344d42ade493f02a09edd99546d21594b18
Kristina Stojic <kristinastojic99@uns.ac.rs>
2021-01-13 12:15:01 +0100

    #sortiranje_predmeta

--- src/izgledAplikacije/ComparatorSortiranjePredmeta.java
+++ src/izgledAplikacije/ComparatorSortiranjePredmeta.java
@@ -8,7 +8,7 @@ public class ComparatorSortiranjePredmeta implements Comparator<String>{
 	@Override
 	public int compare(String s1, String s2) {
 
-		Pattern pattern = Pattern.compile("[A-Za-z0-9]+");
+	 Pattern pattern = Pattern.compile("[A-Za-z0-9]+");
 		
 		if(pattern.matcher(s1).matches() && pattern.matcher(s2).matches()) {
 			
@@ -21,43 +21,13 @@ public class ComparatorSortiranjePredmeta implements Comparator<String>{
 			
 			String slova1 = podjela1[0];
 			String slova2 = podjela2[0];	
-			int br1 = Integer.parseInt(brojevi1);
-			int br2 = Integer.parseInt(brojevi2);
 			
-			
-			/*String[] n = s1.split(""); //array of strings
-		    StringBuffer f = new StringBuffer(); // buffer to store numbers
-
-		    for (int i = 0; i < n.length; i++) {
-		        if((n[i].matches("[0-9]+"))) {// validating numbers
-		            f.append(n[i]); //appending
-		        }  
-		    }
-			
-			
-			
-			String[] n1 = s2.split(""); //array of strings
-		    StringBuffer f1 = new StringBuffer(); // buffer to store numbers
-		    try {
-		    for (int i = 0; i < n1.length; i++) {
-		        if((n1[i].matches("[0-9]+"))) {// validating numbers
-		            f1.append(n1[i]); //appending
-		        }  
-		    }
-			}catch(Exception e) {}
-			*/
-		    
-		    
 			if(!slova1.equals(slova2)) {
-				//System.out.println(slova1 + " " + slova2);
 				return slova1.compareTo(slova2);
 				
 			}else {
-				
-				//System.out.println(brojevi1 + " " + brojevi2);
 				int ret = Integer.parseInt(brojevi1) - Integer.parseInt(brojevi2);
 				return ret;
-				//return brojevi1.compareTo(brojevi2);
 				
 			}
 			
